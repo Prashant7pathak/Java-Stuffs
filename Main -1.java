4afa8507af021f240a43b9a3b8990de217626fd9
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-09-14 14:28:41 +0530

    Add files via upload

---
+++ Main -1.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("To kaise hai Aap log");
+        System.out.println("I hope Achhe honge");
+        int n=3;
+        int m=4;
+        int l=45;
+        int sum = n+m+l;
+        System.out.println(sum);
+        System.out.println("Taking input from user");
+    }
+}
