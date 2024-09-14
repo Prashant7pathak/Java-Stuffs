0ddff0665cc2df24b76d7aef2dd6c7c4c6f8ef93
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-09-14 15:02:53 +0530

    Add files via upload

---
+++ Main-04.java
@@ -0,0 +1,12 @@
+import java.sql.SQLOutput;
+//Questions on Sum of numbers
+public class Main {
+    public static void main(String[] args) {
+        int a =5;
+        int b=45;
+        int c = 69;
+        int sum = a+b+c;
+        System.out.println("The value of sum of given number is:");
+        System.out.println(sum);
+    }
+}
