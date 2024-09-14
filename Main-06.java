42f8e0d4f99188b5221116100852d4158d5246fb
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-09-14 15:24:01 +0530

    Add files via upload

---
+++ Main-06.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+//       Calculation of CGPA
+        float sub1=45;
+        float sub2=45;
+        float sub3=99;
+        float cgpa=(sub1 + sub2 + sub3)/30;
+        System.out.println(cgpa);
+
+    }
+}
