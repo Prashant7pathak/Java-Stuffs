a052e03438e46657bae25818a2ba023a5b062d0d
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-09-14 14:23:10 +0530

    Percentage prob.

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Enter the value");
+        System.out.println("For Percentage Calculation Enter your marks here");
         System.out.println();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of a");
-        int a = sc.nextInt();
-        System.out.println("Enter the value of b");
-        int b = sc.nextInt();
-        int sum = a+b;
-        System.out.println(sum);
+        System.out.println("Enter the Marks of maths");
+        int maths = sc.nextInt();
+        System.out.println("Enter the Marks of IT");
+        int it = sc.nextInt();
+        System.out.println("Enter the Marks of Sst");
+        int sst = sc.nextInt();
+        System.out.println("Enter the Marks of Hindi");
+        int hindi = sc.nextInt();
+        System.out.println("Enter the Marks of Science");
+        int sci = sc.nextInt();
+        int per=((sst+hindi+sci+it+maths)*100)/500;
+        System.out.println(per);
 
     }
 }
