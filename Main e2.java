d0a40f84830fc12bfc0621009593d526ceaebea2
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-12-21 11:09:24 +0530

    Add files via upload

---
+++ Main e2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        -:summation of two number:-
+        int a=20;
+        int b=30;
+        float c = 56.60f;
+        float d= 43.40f;
+
+        System.out.println("sum of number: ");
+        System.out.println(a+b);
+//        -:Average of sum numbers:-
+        System.out.println("Average of these numbers are:");
+        System.out.println((a+b+c+d)/4);
+
+    }
+}
