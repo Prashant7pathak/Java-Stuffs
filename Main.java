e6f5afb5f5d7359e8391e3b9daf839e04ecf2268
Prashant Pathak <144862456+Prashant7pathak@users.noreply.github.com>
2024-12-21 18:58:39 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,14 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("For Percentage Calculation Enter your marks here");
-        System.out.println();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the Marks of maths");
-        int maths = sc.nextInt();
-        System.out.println("Enter the Marks of IT");
-        int it = sc.nextInt();
-        System.out.println("Enter the Marks of Sst");
-        int sst = sc.nextInt();
-        System.out.println("Enter the Marks of Hindi");
-        int hindi = sc.nextInt();
-        System.out.println("Enter the Marks of Science");
-        int sci = sc.nextInt();
-        int per=((sst+hindi+sci+it+maths)*100)/500;
-        System.out.println(per);
-
+        System.out.println("Enter any random number");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        if (num%2==0){
+            System.out.println("This is a Even number");
+        }else{
+            System.out.println("it is a Odd number");
+        }
     }
 }
